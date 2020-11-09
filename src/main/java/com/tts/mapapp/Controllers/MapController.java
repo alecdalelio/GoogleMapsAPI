e9f41ca221d5854da28150f31fe6cf5d6f500a7c
alecdalelio <alecdalelio@gmail.com>
2020-11-09 12:05:32 -0500

    made map interactive with JS/Java

--- src/main/java/com/tts/mapapp/Controllers/MapController.java
+++ src/main/java/com/tts/mapapp/Controllers/MapController.java
@@ -1,12 +1,52 @@
 package com.tts.mapapp.Controllers;
 
+import com.tts.mapapp.Models.Location;
+import com.tts.mapapp.Services.MapService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MapController {
+    
+    @Autowired
+    MapService mapService;
+
     @GetMapping("/home")
-    public String getDefaultMap() {
+    public String getDefaultMap(Model model) {
+        model.addAttribute(new Location());
+        
+        // Location location = new Location();
+        
+        // location.setCity("New Orleans");
+        // location.setState("Louisiana");
+
+        // mapService.addCoordinates(location);
+        // System.out.println(location);
+
         return "index.html";
     }
+
+    @PostMapping("/home")
+    public String getMapForLocation(Location location, Model model) {
+        
+        mapService.addCoordinates(location);
+        model.addAttribute(location);
+
+        return "index";
+    }
+
+    @PostMapping("/random")
+    public String getRandomLocation(Model model){
+
+        Location city = mapService.randomCity();
+
+        model.addAttribute(city);
+
+        return "index";
+    }
+
 }
--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,9 +1,38 @@
 let map;
+let marker;
+let image;
+var contentString = '<h2>' + city + ', ' + state + '</h2>';
 
     function initMap() {
+
+    console.log(google);
+
+    let infowindow = new google.maps.InfoWindow({
+         content: contentString
+    });
+
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 42.8075, lng: -70.8732},
-        zoom: 10
+        center: coords,
+        zoom: 10,
+        scrollwheel: false
+    });
+
+    marker = new google.maps.Marker({
+        position: coords,
+        map: map,
+        icon: image,
+        animation: google.maps.Animation.DROP
+    });
+
+    image = {
+        url: '/location.png', 
+        scaledSize: new google.maps.Size(50, 50)
+    };
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
     });
 }
+
+
     
